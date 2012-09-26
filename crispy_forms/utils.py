4fc4b167882964d880f01fc9f6b13cc03059d378
Ryan Brown <ryan@roastlog.com>
2012-09-26 15:53:22 -0700

    Allow using fields more than once.
    Turned off an exception, replacing it with a logged warning. We need to be able
    to render fields more than once in order to handle <label>s and error messages
    correctly. Responsibility lies on the programmer to not actually render an
    input more than once.

--- crispy_forms/utils.py
+++ crispy_forms/utils.py
@@ -7,7 +7,7 @@ from django.template.loader import get_template
 from django.utils.html import conditional_escape
 
 
-# Global field template, default template used for rendering a field. This way we avoid 
+# Global field template, default template used for rendering a field. This way we avoid
 # loading the template every time render_field is called without a template
 TEMPLATE_PACK = getattr(settings, 'CRISPY_TEMPLATE_PACK', 'bootstrap')
 default_field_template = get_template("%s/field.html" % TEMPLATE_PACK)
@@ -15,7 +15,7 @@ default_field_template = get_template("%s/field.html" % TEMPLATE_PACK)
 def render_field(field, form, form_style, context, template=None, labelclass=None, layout_object=None, attrs=None):
     """
     Renders a django-crispy-forms field
-    
+
     :param field: Can be a string or a Layout object like `Row`. If it's a layout
         object, we call its render method, otherwise we instantiate a BoundField
         and render it using default template 'uni_form/field.html'
@@ -23,7 +23,7 @@ def render_field(field, form, form_style, context, template=None, labelclass=Non
         to avoid double rendering fields.
 
     :param form: The form/formset to which that field belongs to.
-    
+
     :param form_style: A way to pass style name to the CSS framework used.
 
     :template: Template used for rendering the field.
@@ -43,10 +43,10 @@ def render_field(field, form, form_style, context, template=None, labelclass=Non
             if isinstance(field, unicode):
                 field = str(field)
             # If `field` is not unicode then we turn it into a unicode string, otherwise doing
-            # str(field) would give no error and the field would not be resolved, causing confusion 
+            # str(field) would give no error and the field would not be resolved, causing confusion
             else:
                 field = str(unicode(field))
-                
+
         except (UnicodeEncodeError, UnicodeDecodeError):
             raise Exception("Field '%s' is using forbidden unicode characters" % field)
 
@@ -61,12 +61,18 @@ def render_field(field, form, form_style, context, template=None, labelclass=Non
         else:
             field_instance = None
             logging.warning("Could not resolve form field '%s'." % field, exc_info=sys.exc_info())
-            
+
     if not field in form.rendered_fields:
         form.rendered_fields.add(field)
     else:
         if not FAIL_SILENTLY:
-            raise Exception("A field should only be rendered once: %s" % field)
+            """Not sure really how important this is. How about if the
+            programmer is responsible for deciding about rendering a field
+            more than once? I need to do this b/c the label and the field are
+            rendered in different templates. TODO - find a better way!
+            """
+            #raise Exception("A field should only be rendered once: %s" % field)
+            logging.warning("A field should only be rendered once: %s" % field, exc_info=sys.exc_info())
         else:
             logging.warning("A field should only be rendered once: %s" % field, exc_info=sys.exc_info())
 
@@ -92,7 +98,7 @@ def render_field(field, form, form_style, context, template=None, labelclass=Non
 
 def flatatt(attrs):
     """
-    Taken from django.core.utils 
+    Taken from django.core.utils
     Convert a dictionary of attributes to a single string.
     The returned string will contain a leading space followed by key="value",
     XML-style pairs.  It is assumed that the keys do not need to be XML-escaped.
